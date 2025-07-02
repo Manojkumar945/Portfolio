e9d7d3bb418a7d205fc5178146802f6925d919d9
Manojkumar945 <manojk46234@gmail.com>
2025-07-02 22:43:57 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -154,7 +154,7 @@ const Hero = () => {
               fontVariantLigatures: 'common-ligatures contextual',
               wordSpacing: '0.02em'
             }}>
-              Manoj Kumar
+              Mano j Kumar
             </h2>
           </div>
           
