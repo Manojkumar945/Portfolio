1f97a3666d99da9115ddbfc8aa73209f57ea258c
Manojkumar945 <manojk46234@gmail.com>
2026-02-19 12:51:46 +0530

    Updated Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -527,7 +527,7 @@ const Skills = () => {
                               isDarkMode ? 'text-slate-500' : 'text-gray-500'
                             }`}>Project</p>
                             <p className={`text-lg font-semibold ${
-                              isDarkMode ? 'text-dark red-400' : 'text-dark red-600'
+                              isDarkMode ? 'text-white-400' : 'text-white-600'
                             }`}>
                               {award.project}
                             </p>
