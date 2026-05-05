3ace6ffe22f5a44ba3136980ad1620bbba769eaa
Manojkumar945 <manojk46234@gmail.com>
2026-05-05 12:19:15 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -93,7 +93,7 @@ const Hero = () => {
                   paddingBottom: '0.1em',
                   overflow: 'visible'
                 }}>
-                  Manoj Kumar
+                  Manoj Kumar 
                 </h2>
               </div>
               
